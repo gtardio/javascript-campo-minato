2e5b3ad3566ead9eb472f0ecbb75d3b099e0c9f4
Gianluca Tardio <Gianluca.Tardio@gmail.com>
2018-11-21 10:55:04 +0100

    esercizio completato senza bonus difficoltà

--- main.js
+++ main.js
@@ -14,39 +14,103 @@
 // Con difficoltà 0=> da 1 a 100, con difficoltà 1 => da 1 a 80 con
 // difficoltà 2=> da 1 a 50
 
-
-var numeriCpu = [];
-
-for (var i = 0; i < 16; i++) {
-  numeriCpu[i] = Math.floor(Math.random() * 100) + 1;
-}
-console.log(numeriCpu);
-
-var numeroRandom = Math.floor(Math.random() * 100) + 1; // restituisce un numero intero da 1 a 100
-
-var numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 100"));
-console.log(numeroUtente);
-
-if (numeroUtente > 100) {
-  window.alert("Attenzione! Hai inserito un numero maggiore di 100");
+function getRandomNumber (min,max)
+{
+  var newRandomNumber = Math.floor(Math.random() * max-min+1)+min;
+  return newRandomNumber;
 }
 
-// se il numero inserito dall'utente è uguale ad un numero presente nell'array del Cpu allora la partita finisce.
+// voglio cercare un elemento in un array.
+function checkNumber (array, element)
+{
+  var trovato = false; //all'inizio non l'ho trovato
 
-
-for (var i = 0; i < numeriCpu.length; i++) {
-  if (numeriCpu[i] == numeroUtente) {
-    window.alert('La partita finisce qui! Hai inserito un numero già presente in lista');
-  }
-  else if (numeriCpu[i] != numeroUtente) {
-    var newNumero = parseInt(prompt("Grande! Inserisci un altro numero da 1 a 100"));
-    console.log("il tuo nuovo numero è: " + newNumero );
-    if (numeroUtente > 100) {
-      window.alert("Attenzione! Hai inserito un numero maggiore di 100");
+  for (var i = 0; i < array.length; i++)
+  {
+    if (array[i] == element)
+    {
+      trovato = true;
     }
   }
-  else {
-    window.alert('Fine della partita!');
+  return trovato;
+}
 
+var bombe = [];
+
+// il nostro ciclo while deve continuare fino a quando ho 16 numeri nell'array bombe.
+
+while (bombe.length < 16)
+{
+  // generare un nuovo numeroRandom
+  var newCpuNumber = getRandomNumber(1,100);
+
+  // controllare se il numero appena generato è presente nell'array
+  if (bombe.includes(newCpuNumber) == false)  //vuol dire che qui non esiste il numero generato
+  {
+    bombe.push(newCpuNumber);
   }
+}
+console.log(bombe);
+
+// var userNumber = parseInt(prompt('Inserisci un numero da 1 a 100'));
+// var isTrovato = checkNumber(bombe, userNumber);
+//
+// if (isTrovato == true)
+// {
+//   document.writeln('Hai perso');
+// }
+// else
+// {
+//   document.writeln('Hai vinto');
+// }
+
+// le giocate vincenti sono 100 - 16
+// numeri possibili inseriti dall'utente - numero di bombe
+
+// ci creiamo una variabile continua giocare che all'inizio sarò uguale a true
+//questa variabile diventerà false quando: o hai fatto 84 giocate oppure hai trovato una bomba.
+//fino a quel momento, continua a chiedere numeri all'utente e verificare che quei numeri non siano bombe.
+
+var continuaAGiocare = true;
+var giocateVittoriose = 0;
+var numeriUtente = [];
+
+while (continuaAGiocare == true)
+{
+  var userNumber = parseInt(prompt('Inserisci un numero da 1 a 100'));
+  var isTrovatoTraINUmeriUtente = checkNumber(numeriUtente, userNumber);
+  var isTrovato = checkNumber(bombe, userNumber);
+
+  if (isTrovatoTraINUmeriUtente == true)
+  {
+    alert('Inserisci un numero valido')
+  }
+  else
+  { numeriUtente.push(userNumber);
+    if (isTrovato == true)
+    {
+      continuaAGiocare = false;
+    }
+    else
+    {
+      giocateVittoriose = giocateVittoriose + 1 ;
+
+      if (giocateVittoriose >= 84)
+      {
+        continuaAGiocare = false;
+      }
+
+    }
+  }
+
+
+}
+
+if (giocateVittoriose == 84)
+{
+  alert('Hai Vinto!');
+}
+else
+{
+  alert('Hai Perso! Il numero totale di giocate fatte è: ' + giocateVittoriose);
 }
